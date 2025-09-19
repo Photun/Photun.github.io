2d401585537b9c0c4590ae20f2eeb085d8faa2f0
Photun <108767542+Photun@users.noreply.github.com>
2025-09-18 23:32:53 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,8 +1,5 @@
 // small helpers
 export function makeJoinCode(prefix = 'PEP', len = 4) {
-// returns prefix + 4 uppercase alphanum chars
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 let out = '';
-for (let i = 0; i < len; i++) out += chars.charAt(Math.floor(Math.random() * chars.length));
-return prefix + out;
-}
+for (let i = 0; i < len; i++) out += chars.charAt(Math.floor(Math.random() *
